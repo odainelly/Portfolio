77ccfdb471692d6c3ba6037a8f03c1625be9e03d
odainelly <nelyodai7@gmail.com>
2025-11-10 21:15:48 +0000

    update on nav responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,10 +8,10 @@ import { CiLinkedin } from "react-icons/ci";
 
 const Nav = () => {
   return (
-    <nav className=" fixed top-0 left-0 w-full z-[9999] backdrop-blur-md bg-transparent/20 border-b border-white/20 shadow-sm ">
-      <div className='container mx-auto flex sm:justify-start justify-between items-center sm:items-start  py-2 px-auto'>
+    <nav className=" fixed top-0 left-0 w-full z-[9999] backdrop-blur-md bg-transparent/20 border-b border-white/20 shadow-sm sm:max-w-[100%]">
+      <div className='container mx-auto flex sm:justify-start justify-between items-center   py-2 px-auto'>
 
-          <div className=" max-w-7xl mx-auto px-4 py-3 flex items-center justify-between ">
+        <div className=" max-w-7xl mx-auto px-4 py-3 flex items-center justify-between ">
         {/* <Link to="/" className="text-2xl font-bold text-white">
           MyBrand
         </Link> */}
@@ -39,9 +39,10 @@ const Nav = () => {
         </div> */}
 
        
+
       </div>
 
-       <div className=" lg:flex lg:justify-center lg:items-center space-x-2 gap-2 hidden sm:flex">
+       <div className="   space-x-2 gap-2 hidden sm:flex">
 					<NavBarIcon
 						link="https://github.com/odainelly"
 						icon={FaGithub}
